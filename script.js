912421f94af9d8caa767d1daacea1fb17cc23315
Ishika Verma <ishikaverma2317@gmail.com>
2025-08-30 09:18:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your code here
+function updateClock() {
+  const now = new Date();
+
+  const second = now.getSeconds();
+  const minute = now.getMinutes();
+  const hour = now.getHours();
+
+  // calculate angles
+  const secondDeg = (second / 60) * 360;
+  const minuteDeg = (minute / 60) * 360 + (second / 60) * 6;
+  const hourDeg = (hour / 12) * 360 + (minute / 60) * 30;
+
+  document.getElementById("second").style.transform = `rotate(${secondDeg}deg)`;
+  document.getElementById("minute").style.transform = `rotate(${minuteDeg}deg)`;
+  document.getElementById("hour").style.transform = `rotate(${hourDeg}deg)`;
+}
+
+setInterval(updateClock, 1000);
+updateClock();
