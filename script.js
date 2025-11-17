b8e4a358e31ecd9707b6714f002cdceab55d54ad
Ishika Verma <ishikaverma2317@gmail.com>
2025-11-18 03:30:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,18 +9,14 @@ function setClock() {
     const minutes = now.getMinutes();
     const hours   = now.getHours();
 
-    // 6 degrees per second/minute
     const secondDeg = seconds * 6;
-    const minuteDeg = minutes * 6 + seconds * 0.1; 
+    const minuteDeg = minutes * 6 + seconds * 0.1;
     const hourDeg   = (hours % 12) * 30 + minutes * 0.5;
 
-    secondHand.style.transform = `translateX(-50%) rotate(${secondDeg}deg)`;
-    minuteHand.style.transform = `translateX(-50%) rotate(${minuteDeg}deg)`;
-    hourHand.style.transform   = `translateX(-50%) rotate(${hourDeg}deg)`;
+    secondHand.style.transform = `rotate(${secondDeg}deg)`;
+    minuteHand.style.transform = `rotate(${minuteDeg}deg)`;
+    hourHand.style.transform   = `rotate(${hourDeg}deg)`;
 }
 
-// Update clock every second
 setInterval(setClock, 1000);
-
-// Run once on load
 setClock();
